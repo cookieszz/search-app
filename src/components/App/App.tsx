7b3573257ff0cdb3a1b3b00f2fc97f095f4c8a72
cookieszz <maksym.holovko@onix-systems.com>
2020-10-05 14:10:14 +0300

    fix App.styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Redirect, Route, Router, Switch } from "react-router-dom";
 import ContentPage from "../ContentPage/ContentPage";
-import styles from "./App.module.css";
 import { createBrowserHistory } from "history";
 import SearchPage from "../SearchPage/SearchPage";
 import { MuiThemeProvider } from "@material-ui/core";
 import { appTheme } from "../../theme";
 import { BasicRoutes } from "../../constants/basicRoutes";
+import { useAppRootStyles } from "./App.styles";
 
 const appHistory = createBrowserHistory();
 
 function App() {
+  const classes = useAppRootStyles();
+
   return (
     <MuiThemeProvider theme={appTheme}>
-      <div className={styles.appRoot}>
+      <div className={classes.appRoot}>
         <Router history={appHistory}>
           <Switch>
             <Route exact path={BasicRoutes.SEARCH} component={ContentPage} />
