0fb408183774079b7697d6dc7b2042e8731d983f
cookieszz <maksym.holovko@onix-systems.com>
2020-10-07 12:09:49 +0300

    added redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
 export type rootState = ReturnType<typeof rootReducer>;
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
